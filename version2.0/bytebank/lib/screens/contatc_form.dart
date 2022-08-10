a445e02dc7550e3997c05d2fa42b2e832dc5ecbb
athena272 <guilhermera272@gmail.com>
2022-08-09 23:18:59 -0300

    Create contatc_form.dart

---
+++ version2.0/bytebank/lib/screens/contatc_form.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContatcForm extends StatelessWidget {
+  const ContatcForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
